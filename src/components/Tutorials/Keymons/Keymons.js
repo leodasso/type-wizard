216bee017b996e71df441ee459d81b8c08445a19
Leo Dasso <leo.dasso@gmail.com>
2019-03-26 17:21:13 -0500

    add tutorial for keymons

---
+++ src/components/Tutorials/Keymons/Keymons.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Keymons extends Component{
+
+
+	render() {
+		return (
+			<div className="fade-in-panel">
+                <h2>Keymons attack!!</h2>
+                <p>The Keymons have escaped from hell, and are attacking your keyboard! 
+					Press the key they're on to defeat them.
+				</p>
+			</div>
+		);
+	}
+}
+
+export default Keymons;
