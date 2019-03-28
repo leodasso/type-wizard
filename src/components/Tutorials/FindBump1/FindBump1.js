c028867d945f1cdf91faa28c79ba5739d18016e9
Leo Dasso <leo.dasso@gmail.com>
2019-03-28 13:05:32 -0500

    bring image into tutorial

--- src/components/Tutorials/FindBump1/FindBump1.js
+++ src/components/Tutorials/FindBump1/FindBump1.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import tut1Frame1 from '../../../art/tutorial_1_frame_1.png';
+import '../Tutorials.css';
 
 class FindBump1 extends Component{
 
 
 	render() {
 		return (
-			<div className="fade-in-panel">
-                <h2>Let's Get Started!</h2>
-                <p>Find the left-most key with a bump on it.</p>
-				<p>Place your left index finger on it. Press to continue!</p>
+			<div className="fade-in-panel tutorial">
+				<div>
+					<h2>Let's Get Started!</h2>	
+					<p>Find the left-most key with a bump on it.</p>
+					<p>Place your left index finger on it. Press to continue!</p>
+				</div>
+				<img  className="tutorial-image" src={tut1Frame1}/>
 			</div>
 		);
 	}
