a7114d76fedbc8e78dae054254e6bc4a227e574a
Leo Dasso <leo.dasso@gmail.com>
2019-03-28 17:40:40 -0500

    add to dragon level

--- src/data/levels/firstDragon.level.js
+++ src/data/levels/firstDragon.level.js
@@ -3,9 +3,11 @@ import {defaultTutorial} from './shared';
 
 // import chapter classes
 import InitSpawnChapter from '../../classes/chapters/InitSpawnChapter';
+import TimedChapter from '../../classes/chapters/TimedChapter';
 import { beginnerKeys, allKeys } from '../keysets';
 
 import {smallDragon} from '../dragons';
+import basicMonster from '../basicMonster';
 
 
 export default () => new GameLevel(
@@ -32,6 +34,21 @@ export default () => new GameLevel(
             defaultTutorial, 
             undefined, 
             [smallDragon],
+        ),
+
+        new TimedChapter(
+            defaultTutorial, 
+            undefined, 
+            // Init spawns
+            [basicMonster],
+            15,
+            2
+        ),
+
+        new InitSpawnChapter(
+            defaultTutorial, 
+            undefined, 
+            [smallDragon, smallDragon],
         ),
     ]
 )
