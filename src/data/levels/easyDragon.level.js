630d79b6140e986c22272282a3e3a3153e0a62a9
Leo Dasso <leo.dasso@gmail.com>
2019-03-31 15:58:00 -0500

    add easy dragon level

---
+++ src/data/levels/easyDragon.level.js
@@ -0,0 +1,47 @@
+import GameLevel from '../../classes/GameLevel';
+import {defaultTutorial} from './shared';
+
+// import chapter classes
+import InitSpawnChapter from '../../classes/chapters/InitSpawnChapter';
+import TimedChapter from '../../classes/chapters/TimedChapter';
+import { beginnerKeys, allKeys } from '../keysets';
+
+import {smallDragon} from '../dragons';
+import basicMonster from '../basicMonster';
+
+
+export default () => new GameLevel(
+	1,
+	'Words',
+	`Now that you've pressed keys, try typing whole words! Remember to keep your form.`,
+	allKeys,
+
+	// Chapters
+	[
+		new InitSpawnChapter(
+			defaultTutorial, 
+			undefined, 
+			[smallDragon],
+		),
+
+		new InitSpawnChapter(
+			defaultTutorial, 
+			undefined, 
+			[smallDragon],
+		),
+
+		new TimedChapter(
+			defaultTutorial, 
+			undefined, 
+			[basicMonster],
+			15,
+			2
+		),
+
+		new InitSpawnChapter(
+			defaultTutorial, 
+			undefined, 
+			[smallDragon],
+		),
+	]
+)
