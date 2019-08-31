1bec90976385c8da0e8addf2a0665ebdad57681e
Leo Dasso <leo.dasso@gmail.com>
2019-08-31 09:32:54 -0500

    clean up comments

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -9,12 +9,6 @@ import Splash from '../Splash/Splash';
 // Responsible for watching redux state, and returning an appropriate component
 // API for this component is the same as a regular route
 
-// THIS IS NOT SECURITY! That must be done on the server
-// A malicious user could change the code and see any view
-// so your server-side route must implement real security
-// by checking req.isAuthenticated for authentication
-// and by checking req.user for authorization
-
 const ProtectedRoute = (props) => {
   // Using destructuring, this takes ComponentToProtect from component
   // prop and grabs all other props to pass them along to Route
